6df4d0e3410d3cf5f9bc97e61efed843308e759b
dpbhalani <dharmeshbhalani1712@gmail.com>
2023-03-06 19:52:42 +0530

    sorting page's and history data of spacex

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -102,6 +102,7 @@ launches.set(launch.flightNumber, launch);
  function getAllLaunches(skip,limit) {
     return  launchesDatabase.find({},{
         '_id':0 ,'__v':0,})
+        .sort({flightNumber : 1})
         .skip(skip)
         .limit(limit);
 }
